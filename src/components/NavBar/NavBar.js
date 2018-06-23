526837e63619d0f75cab9cfab8d4fd1005951dea
unknown <caylenepicache@gmail.com>
2018-06-22 22:30:59 -0700

    css

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,7 +7,7 @@ class NavBar extends Component {
     return (
       <nav className="navbar navbar-default navbar-fixed-top">
          <ul>
-          <li className="itemLeft">Test Your Memory</li>
+          <li className="itemLeft">Clicky Game</li>
           <li className="itemCenter">Top Score: {this.props.topScore}</li>
           <li className="itemRight">Score: {this.props.score}</li>
         </ul>
